8d262d728c4f0d2ea5227bdbea4bce5a42a97fc5
Sayeed Shaiban <sayeed.k06@gmail.com>
2019-05-18 17:50:22 +0600

    To do list finished

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
 //jshint esversion:6
 const express = require("express");
 const bodyParser = require("body-parser");
-
 const app = express();
+items = [];
+
 app.set('view engine','ejs');
 app.use(bodyParser.urlencoded({extended: true}));// for getting values from forms
+app.use(express.static("public"));
+
+
 app.get("/",function(request,response){
 
-  response.render('test', {foo: 'Mazing'});
+  let today = new Date();
+  let options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+  currentDay = today.toLocaleDateString("en-US",options);
+  response.render('test', {foo: currentDay,listItem: items});
 });
-// app.post("/",function(request,response){
-//   console.log(request.body.test1);
-//   response.send("It works maybe");
-// });
+
+app.post("/",function(req,res){
+  items.push(req.body.addItem);
+  res.redirect('/');
+});
 app.get("/about", function(req,res){
   res.send("Hello this is Shaiban");
 });
